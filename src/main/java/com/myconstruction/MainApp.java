ee91246405cab958efbb2e945854115f85f1f696
DanielCruzCavieres <daniel.cruz@alumno.iplacex.cl>
2025-11-03 18:34:04 -0300

    Create MainApp.java

---
+++ src/main/java/com/myconstruction/MainApp.java
@@ -0,0 +1,7 @@
+package com.myconstruction;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("🚀 Aplicación LoginApp ejecutándose como JAR desde Jenkins y Artifactory");
+    }
+}
